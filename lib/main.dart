c1680f550c630682bcc784fc1ed8cc8f25e2d7dd
azukisiromochi <azukisiromochi@gmail.com>
2021-10-08 19:34:47 +0900

    ✨ ファイル数表示まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,12 @@ class MyApp extends StatelessWidget {
         child: Center(
           child: Builder(builder: (context) {
             return RaisedButton(
-              onPressed: () {
+              // make the function async
+              onPressed: () async {
+                // ### Add the next 2 lines ###
+                final permitted = await PhotoManager.requestPermission();
+                if (!permitted) return;
+                // ######
                 Navigator.of(context).push(
                   MaterialPageRoute(builder: (_) => Gallery()),
                 );
@@ -33,15 +39,42 @@ class Gallery extends StatefulWidget {
 }
 
 class _GalleryState extends State<Gallery> {
+
+  // This will hold all the assets we fetched
+  List<AssetEntity> assets = [];
+
+  @override
+  void initState() {
+    _fetchAssets();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Gallery'),
-      ),
-      body: Center(
-        child: Text("We're gonna display photos here"),
-      ),
+        appBar: AppBar(
+          title: Text('Gallery'),
+        ),
+        body: Center(
+          // Modify this line as follows
+          child: Text('There are ${assets.length} assets'),
+        )
     );
+  }
+
+  _fetchAssets() async {
+    // Set onlyAll to true, to fetch only the 'Recent' album
+    // which contains all the photos/videos in the storage
+    final albums = await PhotoManager.getAssetPathList(onlyAll: true);
+    final recentAlbum = albums.first;
+
+    // Now that we got the album, fetch all the assets it contains
+    final recentAssets = await recentAlbum.getAssetListRange(
+      start: 0, // start at index 0
+      end: 1000000, // end at a very big index (to get all the assets)
+    );
+
+    // Update the state and notify UI
+    setState(() => assets = recentAssets);
   }
 }
